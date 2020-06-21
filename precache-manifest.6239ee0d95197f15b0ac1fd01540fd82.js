b8a81c4b93032ac7d955944d23bd08bcb8348b97
janez <anezjm@gmail.com>
2020-06-21 19:00:20 -0300

    Updates

---
+++ precache-manifest.6239ee0d95197f15b0ac1fd01540fd82.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9863463bab6afd22c9da2cf12f9e12a",
+    "url": "/Layout-challenge/index.html"
+  },
+  {
+    "revision": "d246c88a567610f2e406",
+    "url": "/Layout-challenge/static/css/main.5a330945.chunk.css"
+  },
+  {
+    "revision": "421dfa2c50e3cf479db8",
+    "url": "/Layout-challenge/static/js/2.612e7165.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Layout-challenge/static/js/2.612e7165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d246c88a567610f2e406",
+    "url": "/Layout-challenge/static/js/main.69a75fa7.chunk.js"
+  },
+  {
+    "revision": "6832424b74088d484055",
+    "url": "/Layout-challenge/static/js/runtime-main.fa005825.js"
+  },
+  {
+    "revision": "d22ee3727a7216019c3848df6eafa024",
+    "url": "/Layout-challenge/static/media/github.d22ee372.png"
+  },
+  {
+    "revision": "ab4c1df2dd1ea99b5fca2be5e82f6026",
+    "url": "/Layout-challenge/static/media/photo.ab4c1df2.png"
+  }
+]);
